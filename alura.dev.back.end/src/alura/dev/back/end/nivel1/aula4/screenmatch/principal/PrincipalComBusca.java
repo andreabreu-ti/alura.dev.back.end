01a8494aefba0f2359658cb704fee6b83cb32a3b
andre_abreu <andre_abreu@04B-IN001606.agropalma.net>
2026-05-03 17:00:17 -0300

    API do OMDb

--- alura.dev.back.end/src/alura/dev/back/end/nivel1/aula4/screenmatch/principal/PrincipalComBusca.java
+++ alura.dev.back.end/src/alura/dev/back/end/nivel1/aula4/screenmatch/principal/PrincipalComBusca.java
@@ -5,14 +5,22 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 
+		Scanner leitura = new Scanner(System.in);
+		
+		System.out.println("Digite um filme para busca: ");
+		var busca = leitura.nextLine();
+		
+		String endereco ="https://www.omdbapi.com/?t="+busca+"&apikey=b21ebf4a"; 
+		
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest.newBuilder()
-				.uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=b21ebf4a"))
+				.uri(URI.create(endereco))
 				.build();
 		
 		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
