8bbbfd041af3c92fa7209d63c50bfd185a45b7ad
andre abreu <aanddre@gmail.com>
2026-05-05 22:05:29 -0300

    Encapsulamento

---
+++ alura.dev.back.end/src/alura/dev/back/end/nivel1/aula10/Principal.java
@@ -0,0 +1,20 @@
+package alura.dev.back.end.nivel1.aula10;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Funcionario funcionario = new Funcionario("João", 8500);
+		funcionario.setCargo("Gerente de Projetos");
+
+		System.out.println("Funcionário tem o cargo " + funcionario.getCargo());
+		System.out.println("Funcionário tem o nome " + funcionario.getNome());
+		System.out.println("Funcionário tem o nomesalário" + funcionario.getSalario());
+
+		funcionario.reajustarSalario(5);
+		funcionario.reajustarSalario(10);
+		funcionario.exibirInformacoes();
+
+	}
+
+}
