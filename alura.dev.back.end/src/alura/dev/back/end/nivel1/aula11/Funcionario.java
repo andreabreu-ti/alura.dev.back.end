4140cf759144a9c96830412ca9cef6c7a2fa3236
andre abreu <aanddre@gmail.com>
2026-05-05 22:07:10 -0300

    Praticando Java: herança, polimorfismo e interfaces

---
+++ alura.dev.back.end/src/alura/dev/back/end/nivel1/aula11/Funcionario.java
@@ -0,0 +1,48 @@
+package alura.dev.back.end.nivel1.aula11;
+
+public class Funcionario {
+
+	private String nome;
+	private String cargo;
+	private double salario;
+	private int controleReajuste = 0;
+
+	public Funcionario(String nome, double salario) {
+		this.nome = nome;
+		this.salario = salario;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void exibirInformacoes() {
+
+		System.out.printf("\nFuncionário %s - Cargo: %s - Salário: %.2f", nome, cargo, salario);
+	}
+
+	public void reajustarSalario(double percentual) {
+
+		if (controleReajuste >= 1) {
+		
+			System.out.println("\nNão pode mais fazer reajustes!");
+		} else {
+
+			controleReajuste++;
+			salario += salario * (percentual / 100);
+			System.out.printf("\nNovo salário de %s é de %.2f", nome, salario);
+		}
+	}
+}
